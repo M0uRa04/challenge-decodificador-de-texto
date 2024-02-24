07a6652e6588a0498a15a9ecceff5bc280213d09
Robson <robsonmoura970@hotmail.com>
2024-02-23 21:45:53 -0300

    Feat: Implementing function to encript and decript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 let btnCriptografar = document.getElementById('btn__encript');
+let btnDescriptografar = document.getElementById('btn__decript');
 let transformedText = document.getElementById('transformed_text');
 let btnCopy = document.getElementById('secondary__content__copy__btn');
 let secondaryContainer = document.querySelector('.content__secondary__decripted');
+let textAreaCharacterArray = [];
 let baseEncriptAndDecriptChar = ['a', 'e', 'i', 'o', 'u'];
 let baseEncriptAndDecriptWord = ['ai', 'enter', 'imes', 'ober', 'ufat']
 
@@ -11,39 +13,55 @@ function getTextOfTextArea(textArea) {
     return textValue;
 }
 
-function encripit (getTextOfTextArea) {
-    let textAreaCharacterArray = getTextOfTextArea().split('');
+function removeWordOfString(phrase, word) {
+    let index = phrase.indexOf(word);
+    const wordLength = word.length;
+    while (index !== -1) {
+        const firstLetterIndex = index;
+        phrase = phrase.slice(0, firstLetterIndex + 1) + phrase.slice(firstLetterIndex + wordLength);
+        index = phrase.indexOf(word, firstLetterIndex + 1);
+    }
+    return phrase;
+}
+
+function encripit(getTextOfTextArea) {
+    textAreaCharacterArray = getTextOfTextArea.split('');
     for (let i = 0; i < textAreaCharacterArray.length; i++) {
         for (let j = 0; j < baseEncriptAndDecriptChar.length; j++) {
-            if(textAreaCharacterArray[i] == baseEncriptAndDecriptChar[j]) {
+            if (textAreaCharacterArray[i] === baseEncriptAndDecriptChar[j]) {
                 textAreaCharacterArray[i] = baseEncriptAndDecriptWord[j];
                 break;
-            }           
-        }     
+            }
+        }
     }
     return textAreaCharacterArray.join('');
 }
 
+function decript(text) {
+    let decripitedText = text;
+    for (let i = 0; i < baseEncriptAndDecriptWord.length; i++) {
+        decripitedText = removeWordOfString(decripitedText, baseEncriptAndDecriptWord[i]);
+    }
+    return decripitedText;
+}
 
-btnCriptografar.addEventListener('click', function(){
-    //Pegar o texto
+
+
+
+btnCriptografar.addEventListener('click', function () {
     let encripitedText = encripit(getTextOfTextArea('.textarea__primary__content'));
-    //criptografar o texto
-    /**
-     * Pegar o textValue e transformar em minusculas
-     * Transformar em um array de caracteres
-     * Verificar se nesse array tem as letras a, e , i , o , u
-     * Se tiver substituir pela criptografia correspondente
-     * concluir a criptografia
-     */
-
-    //transferir o texto criptografado para o outro lado da tela.
-    secondaryContainer.style.display="none";
-    transformedText.style.display="flex";
+    secondaryContainer.style.display = "none";
+    transformedText.style.display = "flex";
     transformedText.textContent = encripitedText;
-    btnCopy.style.display="flex";
-    
+    btnCopy.style.display = "flex";
+})
 
+btnDescriptografar.addEventListener('click', function () {
+    let decripitedText = decript(getTextOfTextArea('.textarea__primary__content'));
+    secondaryContainer.style.display = "none";
+    transformedText.style.display = "flex";
+    transformedText.textContent = decripitedText;  
+    btnCopy.style.display = "flex";
 })
 
 
